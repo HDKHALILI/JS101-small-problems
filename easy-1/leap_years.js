c2c142fade5cb8366879c71c791263e1b83890c4
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-05-31 11:36:10 +1000

    add "Further Exploration" of easy-1/leap_years.js

--- easy-1/leap_years.js
+++ easy-1/leap_years.js
@@ -49,16 +49,16 @@ function test(expected, received) {
   return `expected ${expected}, and received ${received}`;
 }
 
-function isLeapYear(year) {
-  if (invalidYear(year)) return "Invalid Year";
-  if (year % 400 === 0) {
-    return true;
-  } else if (year % 4 === 0 && year % 100 !== 0) {
-    return true;
-  }
+// function isLeapYear(year) {
+//   if (invalidYear(year)) return "Invalid Year";
+//   if (year % 400 === 0) {
+//     return true;
+//   } else if (year % 4 === 0 && year % 100 !== 0) {
+//     return true;
+//   }
 
-  return false;
-}
+//   return false;
+// }
 
 // Examples / Test Cases:
 console.log(test(true, isLeapYear(2016))); // true
@@ -75,3 +75,31 @@ console.log(test(false, isLeapYear(1))); // false
 console.log(test(false, isLeapYear(100))); // false
 console.log(test(true, isLeapYear(400))); // true
 console.log(test(true, isLeapYear(0))); // true
+
+// Can you rewrite isLeapYear to perform its tests in the opposite order
+// of the above solution? That is, test whether the year is divisible by 4
+// first, then, if necessary, test whether it is divisible by 100, and
+// finally, if necessary, test whether it is divisible by 400. Is this
+//solution simpler or more complex than the original solution?
+
+function isLeapYear(year) {
+  if (invalidYear(year)) return "Invalid Year";
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
+
+// Verdict:
+// There are too many nested if statements that makes the code
+// harder to read. However, the flow of the logic is a bit clearer or
+// maybe not.
