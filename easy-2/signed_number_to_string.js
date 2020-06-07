dbaa4f499de1bad1bf90c92a4ccea4d015baeec1
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-06-07 15:45:06 +1000

    refactor easy-2/signed_number_to_integer.js

--- easy-2/signed_number_to_string.js
+++ easy-2/signed_number_to_string.js
@@ -37,10 +37,10 @@ function integerToString(number) {
 
 function signedIntegerToString(number) {
   switch (Math.sign(number)) {
-    case -1:
-      return '-' + integerToString(-number);
     case 1:
       return '+' + integerToString(number);
+    case -1:
+      return '-' + integerToString(-number);
     default:
       return integerToString(number);
   }
