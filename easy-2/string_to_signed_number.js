61621fa9bd210f9d6b40d61ebeee609562299e30
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-06-07 14:39:10 +1000

    add a different solution to easy-2/string_to_signed_number.js

--- easy-2/string_to_signed_number.js
+++ easy-2/string_to_signed_number.js
@@ -9,31 +9,71 @@
 
 // You may not use any standard conversion methods available in JavaScript,
 // such as parseInt() and Number().
-const DECI_DIGITS = {
-  0: 0,
-  1: 1,
-  2: 2,
-  3: 3,
-  4: 4,
-  5: 5,
-  6: 6,
-  7: 7,
-  8: 8,
-  9: 9,
-};
-function stringToSignedInteger(string) {
-  let digits = null;
-  if (string[0] === '+' || string[0] === '-') {
-    digits = string.slice(1).split("").map((char) => DECI_DIGITS[char]);
-  } else {
-    digits = string.split('').map((char) => DECI_DIGITS[char]);
-  }
+// const DECI_DIGITS = {
+//   0: 0,
+//   1: 1,
+//   2: 2,
+//   3: 3,
+//   4: 4,
+//   5: 5,
+//   6: 6,
+//   7: 7,
+//   8: 8,
+//   9: 9,
+// };
+// function stringToSignedInteger(string) {
+//   let digits = null;
+//   if (string[0] === '+' || string[0] === '-') {
+//     digits = string.slice(1).split("").map((char) => DECI_DIGITS[char]);
+//   } else {
+//     digits = string.split('').map((char) => DECI_DIGITS[char]);
+//   }
 
+//   let output = 0;
+//   digits.forEach((digit) => {
+//     output = (output * 10) + digit;
+//   });
+//   return string[0] === '-' ? -output : output;
+// }
+
+// console.log(stringToSignedInteger('4321'));
+// console.log(stringToSignedInteger('-570'));
+// console.log(stringToSignedInteger('+100'));
+
+// Let's use stringToInteger() function
+
+function stringToInteger(string) {
+  const DEC_DIGITS = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+  };
+
+  let arrayOfDigits = string.split("").map(char => DEC_DIGITS[char]);
   let output = 0;
-  digits.forEach((digit) => {
+  arrayOfDigits.forEach(digit => {
+    // * 10 is because decimal is base 10
     output = (output * 10) + digit;
   });
-  return string[0] === '-' ? -output : output;
+  return output;
+}
+
+function stringToSignedInteger(string) {
+  switch (string[0]) {
+    case '+':
+      return stringToInteger(string.slice(1));
+    case '-':
+      return -stringToInteger(string.slice(1));
+    default:
+      return stringToInteger(string);
+  }
 }
 
 console.log(stringToSignedInteger('4321'));
