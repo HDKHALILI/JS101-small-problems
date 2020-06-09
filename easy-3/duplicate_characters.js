1d7f337270d08ff66b6219ff10f1b56154148e37
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-06-09 16:25:13 +1000

    add Further Exploration to easy-3/duplicate_characters.js

--- easy-3/duplicate_characters.js
+++ easy-3/duplicate_characters.js
@@ -26,16 +26,16 @@
 
 // Algorithm
 // - output = ''
-// - iterate over each character of the string
+// - iterate over each character of the text
 //  if current char is not the same as next char
 //   - concat it with output
 
 // Code
-function crunch(string) {
+function crunch(text) {
   let output = '';
-  for (let index = 0; index < string.length; index += 1) {
-    if (string[index] !== string[index + 1]) {
-      output += string[index];
+  for (let index = 0; index < text.length; index += 1) {
+    if (text[index] !== text[index + 1]) {
+      output += text[index];
     }
   }
   return output;
@@ -46,3 +46,51 @@ console.log(crunch('4444abcabccba'));              // "4abcabcba"
 console.log(crunch('ggggggggggggggg'));            // "g"
 console.log(crunch('a'));                          // "a"
 console.log(crunch(''));                           // ""
+
+// *** Further Exploration ***
+// You may have noticed that the solution continues iterating until index
+// points past the end of the string. As a result, on the last iteration, text
+// [index] is the last character in text, while text[index + 1] is undefined.
+// Why does it do this? What happens if it stops iterating when index is
+// equal to text.length - 1?
+
+// Answer: final iteration's text[index + 1] = undefined is because at the
+// last iteration the loop has reached the last index, and adding 1 to index
+// will be out of bound. out of bound index returns undefined
+
+// text.length - 1 -> will not work because if the chars be the same
+// they will be ignored. result will have one letter missing.
+// e.g 'yy'[0] === 'yy'[0 + 1] => true and will be ignored.
+
+// It's also possible to solve this using regular expressions. For a nice
+// challenge, give this a try with regular expressions. Can you think of any
+// other solutions that don't use regular expressions?
+
+function crunch2(text) {
+  let crunched = text.split(' ').map(char => char.replace(/(.)\1+/g, '$1'));
+  return crunched.join(' ');
+}
+console.log('------------');
+console.log(crunch2('ddaaiillyy ddoouubbllee'));    // "daily double"
+console.log(crunch2('4444abcabccba'));              // "4abcabcba"
+console.log(crunch2('ggggggggggggggg'));            // "g"
+console.log(crunch2('a'));                          // "a"
+console.log(crunch2(''));                           // ""
+
+function crunch3(text) {
+  if (text.length > 0) {
+    return text.split('').reduce((acc, char) => {
+      if (!acc.endsWith(char)) {
+        acc += char;
+      }
+      return acc;
+    });
+  }
+  return text;
+}
+
+console.log(crunch3('ddaaiillyy ddoouubbllee'));    // "daily double"
+console.log(crunch3('4444abcabccba'));              // "4abcabcba"
+console.log(crunch3('ggggggggggggggg'));            // "g"
+console.log(crunch3('a'));                          // "a"
+console.log(crunch3(''));                           // ""
