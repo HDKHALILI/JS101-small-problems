ed44d946a63c35b1dec1a5be831fefbaaadd536c
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-06-19 17:53:34 +1000

    add Further Exploration to random_age.js

--- easy-4/random_age.js
+++ easy-4/random_age.js
@@ -4,5 +4,23 @@
 
 let minAge = 20;
 let maxAge = 120;
-let age = Math.floor(Math.random() * (maxAge - minAge + 1)) + minAge;
-console.log(age);
+let age = randomBetween(120, 20);
+console.log(`Teddy is ${age} years old!`);
+
+// *** Further Exploration ***
+
+// The randomBetween function used the Math.floor() method. Would it make a
+// difference if the Math.round() method was used instead?
+// Using Math.round will exceed the upper range (we will get 121 at some point)
+
+// Also, how can we make the function more robust? What if the user
+// inadvertently gave the inputs in reverse order (i.e., the value
+// passed to min was greater than max)?
+
+function randomBetween(min, max) {
+  if (min > max) {
+    return Math.floor(Math.random() * (min - max + 1)) + max;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
