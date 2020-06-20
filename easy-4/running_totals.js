c63466533e9a1bc60c832a61e9d2ea2b0c3b04fd
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-06-20 16:18:31 +1000

    add Further Exploration to running_totals.js

--- easy-4/running_totals.js
+++ easy-4/running_totals.js
@@ -25,3 +25,25 @@ console.log(runningTotal([2, 5, 13]));             // [2, 7, 20]
 console.log(runningTotal([14, 11, 7, 15, 20]));    // [14, 25, 32, 47, 67]
 console.log(runningTotal([3]));                    // [3]
 console.log(runningTotal([]));                     // []
+
+// Further Exploration
+
+// Can you rewrite the solution using the Array.prototype.map method? What
+// types of problems do you think are well-suited for the
+// Array.prototype.map method?
+
+function runningTotalWithMap(numbers) {
+  let sum = 0;
+  let result = numbers.map(num => (sum += num));
+  return result;
+}
+
+console.log('------------------');
+console.log(runningTotalWithMap([2, 5, 13]));             // [2, 7, 20]
+console.log(runningTotalWithMap([14, 11, 7, 15, 20]));    // [14, 25, 32, 47, 67]
+console.log(runningTotalWithMap([3]));                    // [3]
+console.log(runningTotalWithMap([]));                     // []
+
+// Array.prototype.map is a great for transformation, where it only relies
+// on its arguments (it doesn't have to reach out for data). Here our solution
+// map is relies on sum variable outside its callback's scope.
