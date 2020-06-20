c760ff19cfdfa801c95876104422e3837d16de3c
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-06-20 16:54:29 +1000

    add part 2 to letter_counter.js

--- easy-4/letter_counter.js
+++ easy-4/letter_counter.js
@@ -55,3 +55,47 @@ console.log(wordSizes('Hey diddle diddle, the cat and the fiddle!'));
 console.log(wordSizes("What's up doc?"));
 // => { "2": 1, "4": 1, "6": 1 }
 console.log(wordSizes('')); // => {}
+
+// Part 2:
+// Modify the wordSizes function to exclude non-letters when determing word size
+// For instance, the word size of "it's" is 3 not 4.
+
+function isLetter(char) {
+  return char >= 'a' && char <= 'z';
+}
+
+function onlyLetters(word) {
+  let cleanedWord = '';
+
+  for (let index = 0; index < word.length; index += 1) {
+    let char = word[index];
+    if (isLetter(char)) {
+      cleanedWord += word[index];
+    }
+  }
+
+  return cleanedWord;
+}
+
+function wordSizes2(text) {
+  let count = {};
+  text.split(' ').forEach(word => {
+    let cleanWord = onlyLetters(word.toLowerCase());
+    let wordSize = cleanWord.length;
+    if (wordSize > 0) {
+      count[wordSize] = count[wordSize] || 0;
+      count[wordSize] += 1;
+    }
+  });
+
+  return count;
+}
+
+console.log('-------------------');
+console.log(wordSizes2('Four score and seven.'));
+// => // { "3": 1, "4": 1, "5": 1, "6": 1 }
+console.log(wordSizes2('Hey diddle diddle, the cat and the fiddle!'));
+// => { "3": 5, "6": 1, "7": 2 }
+console.log(wordSizes2("What's up doc?"));
+// => { "2": 1, "4": 1, "6": 1 }
+console.log(wordSizes2('')); // => {}
