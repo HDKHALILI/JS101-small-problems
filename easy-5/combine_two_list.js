a65a86bc3f62bd34969be7baf9dfe76b7d2e3b73
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-06-22 17:48:54 +1000

    add solution using array map and array reduce method to combine_two_list.js

--- easy-5/combine_two_list.js
+++ easy-5/combine_two_list.js
@@ -34,3 +34,30 @@ function interleave2(array1, array2) {
 
 console.log('with forEach');
 console.log(interleave2([1, 2, 3], ['a', 'b', 'c']));    // [1, "a", 2, "b", 3, "c"]
+
+// Can you solve it using 'map' or 'reduce'?
+// using 'map'
+
+function interleaveMap(array1, array2) {
+  let result = array1.map((item, index) => {
+    return [item, array2[index]];
+  }).flat();
+
+  return result;
+}
+
+console.log(interleaveMap([1, 2, 3], ['a', 'b', 'c']));    // [1, "a", 2, "b", 3, "c"]
+
+
+// using 'reduce'
+function interleaveReduce(array1, array2) {
+  let result = array1.reduce((accumulator, currentElement, index) => {
+    accumulator.push(currentElement, array2[index]);
+    return accumulator;
+  }, []);
+
+  return result;
+}
+
+console.log('using reduce');
+console.log(interleaveReduce([1, 2, 3], ['a', 'b', 'c']));    // [1, "a", 2, "b", 3, "c"]
