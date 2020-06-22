95c6679e6cc26e785a1f4a1ae0ad8b5324abea4e
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-06-22 18:13:05 +1000

    add digit_list.js

---
+++ easy-5/digit_list.js
@@ -0,0 +1,21 @@
+// List of Digits
+// Write a fucntion that takes one argument, a positive integer, and returns a
+// list of digits in the numbers.
+
+// Examples:
+
+// digitList(12345);       // [1, 2, 3, 4, 5]
+// digitList(7);           // [7]
+// digitList(375290);      // [3, 7, 5, 2, 9, 0]
+// digitList(444);         // [4, 4, 4]
+
+function digitList(integers) {
+  let result = String(integers).split('').map(integer => Number(integer));
+
+  return result;
+}
+
+console.log(digitList(12345));       // [1, 2, 3, 4, 5]
+console.log(digitList(7));           // [7]
+console.log(digitList(375290));      // [3, 7, 5, 2, 9, 0]
+console.log(digitList(444));         // [4, 4, 4]
