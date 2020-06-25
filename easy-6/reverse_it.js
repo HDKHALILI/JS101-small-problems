aafe5f9ea94220c80c69c15864190f90187a6d6e
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-06-25 13:00:04 +1000

    add reverseWrods to reverse_it.js

--- easy-6/reverse_it.js
+++ easy-6/reverse_it.js
@@ -16,3 +16,34 @@ function reverseSentence(text) {
 console.log(reverseSentence(''));                       // ""
 console.log(reverseSentence('Hello World'));            // "World Hello"
 console.log(reverseSentence('Reverse these words'));    // "words these Reverse"
+
+
+// Reverse It (Part 2)
+
+// Write a function that takes a string argument containing one or more words,
+// and returns a new string containing the words from the string argument. All
+// five-or-more letter words should have their letters in reverse order. The
+// string argument will consist of only letters and spaces. Words will be
+// separated by a single space.
+
+// Examples:
+
+// reverseWords('Professional');             // "lanoisseforP"
+// reverseWords('Walk around the block');    // "Walk dnuora the kcolb"
+// reverseWords('Launch School');            // "hcnuaL loohcS"
+
+function reverseWords(text) {
+  let result = text.split(' ').map(word => {
+    if (word.length >= 5) {
+      return word.split('').reverse().join('');
+    }
+
+    return word;
+  });
+
+  return result.join(' ');
+}
+
+console.log(reverseWords('Professional'));             // "lanoisseforP"
+console.log(reverseWords('Walk around the block'));    // "Walk dnuora the kcolb"
+console.log(reverseWords('Launch School'));            // "hcnuaL loohcS"
